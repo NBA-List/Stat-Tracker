9cbe5eadd7e37820d351910c91e3547f877d3969
Aiden Blinn <apblinn@gmail.com>
2022-03-05 10:21:31 -0600

    file architecture:

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App.js";
+
+import styles from "./styles/application.scss";
+
+render(<App />, document.getElementById("root"));
