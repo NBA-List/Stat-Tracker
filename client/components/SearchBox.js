d0e02512585319557ffdd2346b5247a100794bf1
Ian <ianfmadden@gmail.com>
2022-03-05 19:40:06 -0600

    Team dropdown working, not player dropdown.
    Co-authored-by: ericlay14 <ericlay14@users.noreply.github.com>

--- client/components/SearchBox.js
+++ client/components/SearchBox.js
@@ -1,19 +1,32 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
 
+let teamPlayerVitals;
+const teamPlayerNames = [];
+
 class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+    this.makePlayerDropdown = this.makePlayerDropdown.bind(this);
+  }
+
   componentDidMount() {
     fetch('https://api-nba-v1.p.rapidapi.com/teams', {
       headers: {
-        // 'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
-        // 'x-rapidapi-key': '135fde008amsh21b6fc09093bbd5p172cf7jsn085e38b2c831',
+        'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
+        'x-rapidapi-key': '76befa362emshbec67c1a0062f13p17e6b0jsn113a7fce6386',
       },
     })
       .then((data) => data.json())
       .then((data) => {
-        console.log('hi');
-        console.log(data);
-        this.makeTeamDropdown(data);
+        const nbaTeams = [];
+        data.response.forEach((team) => {
+          if ((team.nbaFranchise === true) && ((team.name !== 'Home Team Stephen A'))) {
+            nbaTeams.push([team.id, team.name]);
+          }
+        });
+
+        this.makeTeamDropdown(nbaTeams);
       });
   }
 
@@ -24,15 +37,20 @@ class SearchBox extends Component {
       return;
     }
     document.getElementById('player-names').disabled = false;
-    fetch('https://api-nba-v1.p.rapidapi.com/players', {
-      params: { team: teamId, season: '2021' },
+    fetch('https://api-nba-v1.p.rapidapi.com/players?team=6&season=2021', {
       headers: {
         'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com',
-        'x-rapidapi-key': '135fde008amsh21b6fc09093bbd5p172cf7jsn085e38b2c831',
+        'x-rapidapi-key': '76befa362emshbec67c1a0062f13p17e6b0jsn113a7fce6386',
       },
     })
       .then((data) => data.json())
-      .then((data) => this.makePlayerDropdown(data));
+      .then((data) => {
+        teamPlayerVitals = data.response;
+        teamPlayerVitals.forEach((player) => {
+          teamPlayerNames.push([player.id, player.firstname.concat(' ', player.lastname)]);
+        });
+        this.makePlayerDropdown(teamPlayerNames);
+      });
   }
 
   makePlayerDropdown(data) {
@@ -40,18 +58,16 @@ class SearchBox extends Component {
     console.log(data);
     data.forEach((player) => {
       const option = document.createElement('option');
-      option.value = player.id;
-      option.innerText = `${player.firstname} ${player.lastname}`;
+      [option.value, option.innerText] = player;
       document.getElementById('player-names').appendChild(option);
     });
   }
 
   makeTeamDropdown(data) {
     data.forEach((team) => {
-      if (!team.nbaFranchise || team.id === 37) return;
+      // if (!team.nbaFranchise || team.id === 37) return;
       const option = document.createElement('option');
-      option.value = team.id;
-      option.innerText = team.name;
+      [option.value, option.innerText] = team;
       document.getElementById('team-names').appendChild(option);
     });
   }
@@ -62,10 +78,11 @@ class SearchBox extends Component {
         <label htmlFor="team-names">Choose a team: </label>
         <select onChange={this.getPlayers} name="team-names" id="team-names">
           <option value="Choose" selected="selected">Choose a team:</option>
+          {teamPlayerNames}
         </select>
         <br />
         <label htmlFor="player-names">Choose a player: </label>
-        <select onChange={this.getPlayers} name="player-names" disabled="true" id="player-names">
+        <select name="player-names" disabled="true" id="player-names">
           <option value="Choose" selected="selected">Choose a player:</option>
         </select>
       </>
