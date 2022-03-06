78bb22e2296bef4d5fe1b5267fa5dfedc83c4347
Ian <ianfmadden@gmail.com>
2022-03-06 17:24:51 -0600

    aiden's changes merged

--- client/containers/Dashboard.js
+++ client/containers/Dashboard.js
@@ -1,15 +1,97 @@
-import React, { Component } from "react";
-import Team from "../components/Team";
-import Player from "../components/Player";
+import React, { Component } from 'react';
+import Team from '../components/Team';
+import Player from '../components/Player';
 
 class Dashboard extends Component {
   render() {
     return (
-      <>
-          <Player />
-          <Team />
-      </>
-    )
+      <div id="dashboard">
+        <Player />
+        <Team />
+        <p style={{ fontSize: '30px' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+          consequat lobortis lacus a condimentum. Fusce et viverra dui. In
+          mauris metus, sagittis eu vulputate ut, molestie quis quam. Integer
+          non mauris interdum, mollis lorem eleifend, porttitor quam. Aliquam
+          aliquet est eu leo rutrum condimentum. Suspendisse justo nunc,
+          ultricies ac est vel, facilisis tincidunt arcu. Suspendisse sed magna
+          risus. Quisque ac eros varius, tempus mauris a, ultricies urna.
+          Integer mollis nulla ac mauris dictum, quis facilisis nunc eleifend.
+          Suspendisse non facilisis nisi. Sed aliquet, velit a eleifend
+          faucibus, leo velit accumsan nisi, id gravida nisl libero id nisl.
+          Etiam maximus nibh dictum ipsum viverra euismod. In hac habitasse
+          platea dictumst. Vestibulum elementum vehicula metus, sed facilisis
+          quam vestibulum ac. Proin vitae justo ut nunc rutrum euismod. Nullam
+          id ipsum volutpat, tempor orci dapibus, pharetra lectus. Mauris
+          facilisis mattis quam et ornare. Sed accumsan congue orci et
+          hendrerit. Curabitur vulputate vitae leo a auctor. Phasellus elementum
+          ultrices tortor sit amet sagittis. Cras dignissim nulla ut dolor
+          consequat, tristique egestas tortor imperdiet. Vivamus eget purus sit
+          amet lorem pellentesque fermentum. Suspendisse placerat orci vitae
+          tincidunt aliquam. Fusce ullamcorper egestas ornare. Proin maximus
+          sollicitudin tincidunt. Interdum et malesuada fames ac ante ipsum
+          primis in faucibus. Sed fermentum iaculis pulvinar. Nulla commodo est
+          eu tortor maximus vehicula. Suspendisse sed justo euismod quam pretium
+          accumsan in et odio. Praesent sed pulvinar odio. Phasellus vulputate
+          sollicitudin mi, ac pulvinar lectus facilisis sed. Donec ullamcorper
+          tristique erat, in tempor tortor eleifend in. Maecenas imperdiet
+          iaculis mattis. Class aptent taciti sociosqu ad litora torquent per
+          conubia nostra, per inceptos himenaeos. Donec commodo lorem mauris.
+          Integer est augue, dapibus ornare nisi in, vulputate hendrerit dolor.
+          Pellentesque sit amet metus at libero ullamcorper tristique porttitor
+          at ante. Nulla sit amet nisl eu mauris tempor elementum ac vel lorem.
+          Integer feugiat accumsan magna, et molestie leo aliquam sed. In
+          bibendum magna ut libero gravida posuere. Sed metus diam, fermentum id
+          fermentum sit amet, placerat a lorem. Nam ornare neque placerat erat
+          varius dignissim. Maecenas et velit ac turpis ultricies accumsan in et
+          justo. Sed id dolor ut tortor vulputate fermentum quis quis enim.
+          Nullam vitae pellentesque dolor. Ut ornare dui eget varius sagittis.
+          Suspendisse cursus bibendum ipsum. Sed eget tempor libero, at
+          pellentesque ipsum. Interdum et malesuada fames ac ante ipsum primis
+          in faucibus. Pellentesque nisi urna, hendrerit sed auctor non,
+          molestie a velit. Vestibulum ante ipsum primis in faucibus orci luctus
+          et ultrices posuere cubilia curae; Proin congue lorem mi, sed dapibus
+          augue fermentum in. Morbi in nisl sodales, eleifend quam a, dignissim
+          turpis. Ut tincidunt ligula sed dolor semper ultrices. Curabitur sit
+          amet eleifend nunc. Quisque malesuada congue sem, id facilisis orci
+          pellentesque ut. Mauris viverra nisl at tortor iaculis, pharetra
+          efficitur dolor vehicula. In ultrices ligula eget ex ornare lobortis.
+          Aliquam viverra urna velit, non commodo magna rutrum ac. Aliquam ut
+          magna diam. Maecenas luctus quis odio id rhoncus. Nullam dictum in
+          diam id rhoncus. Mauris ante lectus, accumsan non facilisis a, sodales
+          in odio. Aliquam nec convallis nibh. Fusce iaculis arcu mollis ipsum
+          auctor, in dignissim purus accumsan. Nam tincidunt lectus eros, eu
+          suscipit nisi cursus quis. Maecenas at enim ex. Nam fringilla diam
+          eget libero pretium rhoncus. Etiam at pellentesque sapien, eget cursus
+          orci. Cras viverra sed urna ut tincidunt. Proin nisi felis, semper nec
+          neque et, dictum imperdiet tortor. Etiam mollis consectetur metus, et
+          sodales nulla aliquam a. Nunc auctor, ante at pulvinar tempor, elit
+          dolor tempus justo, et faucibus tortor lorem sed ex. Duis a blandit
+          justo. Suspendisse accumsan ipsum ac molestie egestas. Vestibulum ante
+          ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Sed quis sodales justo, condimentum interdum ligula. Nullam sem
+          tellus, rhoncus consequat congue et, molestie quis nisi. Morbi
+          consequat ipsum sollicitudin tristique consectetur. Proin augue diam,
+          rhoncus nec enim vitae, iaculis maximus tellus. Quisque suscipit
+          ligula a scelerisque egestas. Phasellus tempor sagittis tristique. Ut
+          et metus at justo malesuada semper. Nulla ullamcorper vulputate urna.
+          In lacus neque, semper sed fringilla rutrum, eleifend sed massa.
+          Pellentesque dictum aliquet commodo. Nulla id molestie dui. Donec sit
+          amet dictum lectus. Nam eu mi vel metus luctus semper vel sit amet
+          erat. Quisque ut diam laoreet est dapibus condimentum eget vitae
+          nulla. Etiam tristique odio vel ipsum scelerisque condimentum. Ut
+          vitae dolor dolor. In hac habitasse platea dictumst. Nam varius eu
+          augue ac accumsan. Pellentesque habitant morbi tristique senectus et
+          netus et malesuada fames ac turpis egestas. Cras ut pulvinar mauris.
+          Maecenas euismod arcu tellus, eget tempor purus cursus at. Praesent
+          mauris ante, laoreet at metus in, consequat dapibus dui. Sed eu sem
+          quam. Ut pulvinar pharetra lectus et eleifend. Vestibulum quis est non
+          ante euismod pretium vitae id enim. Maecenas bibendum sapien et
+          sollicitudin feugiat. Sed gravida, enim et sagittis laoreet, est
+          mauris ullamcorper massa, id tempus sapien urna eu magna.
+        </p>
+      </div>
+    );
   }
 }
 
