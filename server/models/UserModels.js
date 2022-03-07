1fd39fb09bb2f9664326b2511159e5da473d7a5d
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2022-03-06 22:42:15 -0600

    finish google oauth backend

--- server/models/UserModels.js
+++ server/models/UserModels.js
@@ -1,36 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const MONGO_URI = 'mongodb+srv://ianfmadden:Password123@cluster0.urrq0.mongodb.net/Cluster0?retryWrites=true&w=majority';
+const MONGO_URI =
+  "mongodb+srv://ianfmadden:Password123@cluster0.urrq0.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
-mongoose.connect(MONGO_URI, {
+mongoose
+  .connect(MONGO_URI, {
     // options for the connect method to parse the URI
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'userData',
+    dbName: "userData",
     // sets the name of the DB that our collections are part of
   })
-    .then(() => console.log('Connected to Mongo DB.'))
-    .catch((err) => console.log(err));
-  
-  const { Schema } = mongoose;
-  
-  // sets a schema for the 'species' collection
-  const userSchema = new Schema({
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
+  .then(() => console.log("Connected to Mongo DB."))
+  .catch((err) => console.log(err));
+
+const { Schema } = mongoose;
+
+// sets a schema for the 'species' collection
+const userSchema = new Schema({
+  name: {
     type: String,
     required: true,
-    },
-    profilePic: {
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  profilePic: {
     type: String,
     default: "",
-    },
-    favorited_teams: [String],
-    favorited_players: [String],
-  });
-  
-  // creats a model for the 'users' collection that will be part of the export
-  module.exports = mongoose.model("user", userSchema)
+  },
+  favorited_teams: [String],
+  favorited_players: [String],
+});
+
+// creats a model for the 'users' collection that will be part of the export
+module.exports = mongoose.model("user", userSchema);
