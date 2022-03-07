d0091529563439c5b9cdc5d516d24c7182721fd6
Aiden Blinn <apblinn@gmail.com>
2022-03-07 00:09:50 -0600

    working on add team/player

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,6 @@
-const path = require("path");
-const User = require("../models/UserModels");
-const { OAuth2Client } = require("google-auth-library");
+const path = require('path');
+const User = require('../models/UserModels');
+const { OAuth2Client } = require('google-auth-library');
 
 const UserController = {
   // Create a new user in the Database
@@ -25,19 +25,89 @@ const UserController = {
     User.create(
       {
         name: req.body.name,
-        favorited_teams: req.body.teams,
-        favorited_players: req.body.players,
+        email,
       },
       (err, data) => {
         if (err) {
           return res.sendStatus(404);
         }
-        console.log("here");
+        console.log('here');
 
         res.locals.createdUser = data;
         return next();
       }
     );
+  },
+
+  addTeam(req, res, next) {
+    console.log(req.body);
+    const teamId = req.body.teamId;
+    const query = {};
+    query.favorited_teams = teamId;
+    User.findOneAndUpdate(
+      { email: req.cookies.email },
+      { $push: query },
+      (err, user) => {
+        if (err) {
+          return next({
+            log: 'Error in addTeam middleware',
+            message: { err: 'An error occurred while trying to add a team' },
+          });
+        }
+        return next();
+      }
+    );
+  },
+
+  getTeams(req, res, next) {
+    User.findOne({ email: req.cookies.email }, (err, user) => {
+      if (err) {
+        return next({
+          log: 'Error in getTeams middleware',
+          message: {
+            err: 'An error occurred while trying to get teams',
+          },
+        });
+      }
+      res.locals.teams = user.favorited_teams;
+      return next();
+    });
+  },
+
+  addPlayer(req, res, next) {
+    const playerId = req.body.playerId;
+    const query = {};
+    query.favorited_players = playerId;
+    User.findOneAndUpdate(
+      { email: req.cookies.email },
+      { $push: query },
+      (err, user) => {
+        if (err) {
+          return next({
+            log: 'Error in addPlayer middleware',
+            message: {
+              err: 'An error occurred while trying to add a player',
+            },
+          });
+        }
+        return next();
+      }
+    );
+  },
+
+  getPlayers(req, res, next) {
+    User.findOne({ email: req.cookies.email }, (err, user) => {
+      if (err) {
+        return next({
+          log: 'Error in getPlayers middleware',
+          message: {
+            err: 'An error occurred while trying to get players',
+          },
+        });
+      }
+      res.locals.players = user.favorited_players;
+      return next();
+    });
   },
 };
 
